9712f773c98fc228d423d5f8cbd33adc7f8efe76
Richard Garner <richardgarner@mac.com>
2018-08-20 19:02:08 -0500

    boxes are now generated aligned side by side

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var button = document.createElement("button");
-var buttText = document.createTextNode("add square")
+let button = document.createElement("button");
+let buttText = document.createTextNode("add square")
 button.appendChild(buttText)
 document.body.appendChild(button)
+let boxDiv = document.createElement("div");
+boxDiv.className = "flex"
+document.body.appendChild(boxDiv)
 
 let boxNumber = 0;
 button.addEventListener("click", function () {
-    boxNumber++
+    let boxID = boxNumber++
     let box = document.createElement('div');
     box.className = 'Box';
-    box.id = boxNumber;
-    document.body.appendChild(box)
-})
+    box.id = boxID;
+    boxDiv.appendChild(box)
+});
